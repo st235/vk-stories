eea2cd69e4162c62b9f59ffbb35c4c9469bdefae
alexander <st235@yandex.ru>
2017-09-09 23:45:08 +0300

    Implemented tabs

--- app/src/main/java/com/github/sasd97/vk_stories/presentation/StoryActivity.java
+++ app/src/main/java/com/github/sasd97/vk_stories/presentation/StoryActivity.java
@@ -1,5 +1,6 @@
 package com.github.sasd97.vk_stories.presentation;
 
+import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -19,6 +20,7 @@ public class StoryActivity extends AppCompatActivity {
     @Inject StickerProvider stickerProvider;
 
     private View smileButton;
+    private TabLayout tabLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +33,17 @@ public class StoryActivity extends AppCompatActivity {
                 .inject(this);
 
         smileButton = findViewById(R.id.smileButton);
+        tabLayout = findViewById(R.id.tabLayout);
+
         smileButton.setOnClickListener(view -> {
             StickerSheet stickerSheet = new StickerSheet();
             stickerSheet.setProvider(stickerProvider);
             stickerSheet.show(getSupportFragmentManager(), null);
         });
+
+        String[] tabTitles = getResources().getStringArray(R.array.tabs_titles);
+        for (String tabTitle: tabTitles) {
+            tabLayout.addTab(tabLayout.newTab().setText(tabTitle));
+        }
     }
 }
